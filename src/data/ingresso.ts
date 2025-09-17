5d76e770c6779441d2575f29e8a51a76ea0e629d
desenvolvimentoNossafaculdade.NathanAlencar <desenvolvimento.nossafaculdade@gmail.com>
2025-09-17 10:06:51 -0300

    fix: ingressos congresso

--- src/data/ingresso.ts
+++ src/data/ingresso.ts
@@ -5,33 +5,47 @@ export interface IngressoItem {
   title: string;
   preco: string;
   parcelas: string | null;
-  benefits: string[]; // Lista de benefícios do ingresso
-  logoSrc?: string; // Caminho para a logo do ingresso no card
+  benefits: string[];
+  logoSrc?: string;
+  symplaLink: string;
 }
 
 export const ingressos: IngressoItem[] = [
   {
     id: 'passaporte-lote-0',
-    title: 'PASSAPORTE LOTE 0',
-    preco: 'R$ 200,00',
-    parcelas: '12x de R$ 20,00',
-    benefits: ['Acesso a dois dias de evento', 'Welcome kit', 'Certificado de participação'],
-    logoSrc: '/images/LogoSimbolo.png', // Caminho da logo
+    title: 'Passaporte',
+    preco: 'R$ 247,00',
+    parcelas: 'em até 12x',
+    benefits: ['Acesso a dois dias de evento', 'Certificado de participação', 'Welcome kit'],
+    logoSrc: '/images/LogoSimbolo.png',
+    symplaLink: 'https://www.sympla.com.br/evento/i-congresso-caririense-de-saude-integrada/3027154?_gl=1*ty4lem*_gcl_au*MTUzNzI2MzY0Mi4xNzU4MDQ1MDc4*_ga*MTMwNjYxODE5OC4xNzU4MDQ1MDc4*_ga_KXH10SQTZF*czE3NTgwNDUwNzckbzEkZzEkdDE3NTgwNDU0NjYkajYwJGwwJGg0NzA4ODIzMjc'
   },
   {
     id: 'passaporte-meia-lote-0',
-    title: 'PASSAPORTE MEIA LOTE 0',
-    preco: 'R$ 100,00',
-    parcelas: '12x de R$ 10,00',
-    benefits: ['Acesso a dois dias de evento', 'Welcome kit', 'Certificado de participação'],
+    title: 'Passaporte (meia-entrada)',
+    preco: 'R$ 123,50',
+    parcelas: 'em até 12x',
+    benefits: ['Acesso a dois dias de evento', 'Certificado de participação', 'Welcome kit'],
     logoSrc: '/images/LogoSimbolo.png',
+    symplaLink: 'https://www.sympla.com.br/evento/i-congresso-caririense-de-saude-integrada/3027154?_gl=1*ty4lem*_gcl_au*MTUzNzI2MzY0Mi4xNzU4MDQ1MDc4*_ga*MTMwNjYxODE5OC4xNzU4MDQ1MDc4*_ga_KXH10SQTZF*czE3NTgwNDUwNzckbzEkZzEkdDE3NTgwNDU0NjYkajYwJGwwJGg0NzA4ODIzMjc'
   },
   {
     id: 'somente-palestras-lote-0',
-    title: 'INGRESSO SOMENTE PALESTRAS LOTE 0',
-    preco: 'R$ 100,00',
-    parcelas: '12x de R$ 10,00',
-    benefits: ['Acesso a palestras do Palco Principal', 'Certificado de participação'],
+    title: 'Ingresso Coletivo',
+    preco: 'R$ 99,99',
+    parcelas: 'em até 12x',
+    benefits: ['Acesso a dois dias de evento', 'Certificado de participação', 'Welcome kit'],
     logoSrc: '/images/LogoSimbolo.png',
+    symplaLink: 'https://www.sympla.com.br/evento/i-congresso-caririense-de-saude-integrada/3027154?_gl=1*ty4lem*_gcl_au*MTUzNzI2MzY0Mi4xNzU4MDQ1MDc4*_ga*MTMwNjYxODE5OC4xNzU4MDQ1MDc4*_ga_KXH10SQTZF*czE3NTgwNDUwNzckbzEkZzEkdDE3NTgwNDU0NjYkajYwJGwwJGg0NzA4ODIzMjc'
   },
+  {
+    id: 'somente-professore-CNP',
+    title: 'Ingresso exclusivo para professores CNP',
+    preco: 'R$ 00,00',
+    parcelas: '-',
+    benefits: ['Acesso a dois dias de evento', 'Certificado de participação', 'Welcome kit'],
+    logoSrc: '/images/LogoSimbolo.png',
+    symplaLink: 'https://www.conapro.org/professor/criar'
+  },
+
 ];
